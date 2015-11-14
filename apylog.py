563b0260fe7c4841885300f6a22b7ba0dad44ac6
Ivan Medvedev <medvedev.ivan@mail.ru>
2015-11-14 16:51:47 +0300

    Add files to repo
    Add files to repo

---
+++ apylog.py
@@ -0,0 +1,6 @@
+﻿import os, datetime
+
+# severity: 0 - Info, 1 - Warning, 2 - Error
+def AddMessage(severity,message):
+	sd = {0: 'Info', 1: 'Warning', 2: 'Error'}
+	print "{0} at {1}: {2}".format(sd[severity],datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S'),message)
